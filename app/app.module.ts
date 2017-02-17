eaf68bb0c1066ddca265e9d080abe39f77926165
Bhushan Goel <bhushan.goel@paytm.com>
2017-02-17 19:18:34 +0530

    setup basic routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,17 +8,19 @@ import {FormsModule} from '@angular/forms';
 //components
 import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
+import {ManageComponent} from './components/manage/manage.component';
 
 //Routes
 const appRoutes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'manage', component: ManageComponent},
     {path: '**', component: HomeComponent}
 ];
 
 //decorator
 @NgModule({
     imports: [BrowserModule, RouterModule.forRoot(appRoutes), HttpModule, FormsModule],
-    declarations: [AppComponent, HomeComponent],
+    declarations: [AppComponent, HomeComponent, ManageComponent],
     bootstrap: [AppComponent]
 })
 
