f1aeb30a07303f0b28f3f6820c4c48b6d6d34b02
Bhushan Goel <bhushan.goel@paytm.com>
2017-02-17 22:47:58 +0530

    adding manage component

---
+++ app/components/manage/manage.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'manage',
+    templateUrl: 'app/components/manage/manage.component.html'
+})
+
+export class ManageComponent {
+    constructor() {
+
+    }
+}
---
+++ app/main.js
@@ -0,0 +1,8 @@
+//JIT (Just In Time) compiler
+//this is the default setup for JIT compilation
+//TODO: read about JIT and AOT(Ahead of Time)
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_module_1 = require('./app.module');
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
