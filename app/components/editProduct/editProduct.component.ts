208b5de2865a1ae806245dbb3ec055d74e850eba
bhushangoel <bgoel1991@gmail.com>
2017-02-24 13:21:25 +0530

    edit products component

---
+++ app/components/editProduct/editProduct.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'editproduct',
+    template: '<addproduct></addproduct>'
+})
+
+export class EditProductComponent {
+    constructor() {
+    }
+}
